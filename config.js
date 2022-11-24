a1c41ff3ede045e8a67711b77c1ff1051086e181
zallmodder <117575996+zallmodder@users.noreply.github.com>
2022-11-24 19:14:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'Zidann'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281770447981']
+global.premium = ['6281770447981']
+global.packname = 'Zidann'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
